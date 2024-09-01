38756cf72324e8bdbd4fe0ee38525f80fc3f262c
Vahid <vahid.tahmasebi95@gmail.com>
2024-09-01 12:19:14 +0330

    feat: add header component

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,5 +1,6 @@
-import vazirFont from "@/constants/localFont";
 import "@/styles/globals.css";
+import vazirFont from "@/constants/localFont";
+import Header from "@/components/Header";
 
 export const metadata = {
   // title: "بلاگ اپ",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${vazirFont.variable} font-sans min-h-screen`}>
+        <Header />
         <div className="container xl:max-w-screen-xl"> {children}</div>
       </body>
     </html>
