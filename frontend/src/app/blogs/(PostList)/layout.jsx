911a6e6cbe92c67618429d62f339011496bb8f8b
Vahid <vahid.tahmasebi95@gmail.com>
2024-09-04 13:22:16 +0330

    feat: add category list

--- frontend/src/app/blogs/(PostList)/layout.jsx
+++ frontend/src/app/blogs/(PostList)/layout.jsx
@@ -1,3 +1,11 @@
+import { Suspense } from "react";
+import CategoryList from "../_components/CategoryList";
+import Spinner from "@/ui/Spinner";
+
+export const metadata = {
+  title: "بلاگ ها",
+};
+
 async function Layout({ children }) {
   return (
     <div>
@@ -6,7 +14,9 @@ async function Layout({ children }) {
       </h1>
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-12 lg:col-span-4 xl:col-span-3 space-y-4 text-secondary-500">
-          Category List
+          <Suspense fallback={<Spinner />}>
+            <CategoryList />
+          </Suspense>
         </div>
         <div className="col-span-12 lg:col-span-4 xl:col-span-9">
           {children}
@@ -15,4 +25,5 @@ async function Layout({ children }) {
     </div>
   );
 }
+
 export default Layout;
